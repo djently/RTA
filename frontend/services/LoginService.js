8dabfd1743b4a24922501eeb0d88856352db3ead
Stanislav Kovalev <djently@yandex.ru>
2016-04-09 22:19:40 +0300

    refactor LoginService

--- frontend/services/LoginService.js
+++ frontend/services/LoginService.js
@@ -1,35 +1,41 @@
+var _private = {};
+
 class LoginService {
     constructor($rootScope, $sessionStorage, SocketService, EVENTS) {
-        this.$rootScope = $rootScope;
-        this.SocketService = SocketService;
-        this.$storage = $sessionStorage;
-        this.EVENTS = EVENTS;
+        _private.$rootScope = $rootScope;
+        _private.SocketService = SocketService;
+        _private.$storage = $sessionStorage;
+        _private.EVENTS = EVENTS;
 
-        if (this.$storage.user && this.$storage.user.login) {
-            this.login(this.$storage.user.login);
+        let $storage = $sessionStorage;
+
+        if ($storage.user && $storage.user.login) {
+            this.login($storage.user.login);
         }
 
-        this.SocketService.on(
+        SocketService.on(
             SocketService.events.loggedIn,
             this.loggedIn.bind(this)
         );
     }
 
     login(login) {
-        this.SocketService.emit(this.SocketService.events.login, login);
+        _private.SocketService.emit(_private.SocketService.events.login, login);
     }
 
     loggedIn(user) {
-        this.$storage.user = user;
+        _private.$storage.user = user;
         this.user = user;
-        this.$rootScope.$emit(this.EVENTS.LOGGED_IN, user);
-        this.$rootScope.$emit(this.EVENTS.USER_UPDATE, user);
+        _private.$rootScope.$emit(_private.EVENTS.LOGGED_IN, user);
+        _private.$rootScope.$emit(_private.EVENTS.USER_UPDATE, user);
     }
 
     logout() {
-        this.SocketService.emit(this.SocketService.events.logout, this.user);
-        this.$rootScope.$emit(this.EVENTS.LOGGED_OUT, this.user);
-        delete this.$storage.user;
+        _private.SocketService.emit(
+            _private.SocketService.events.logout, this.user
+        );
+        _private.$rootScope.$emit(_private.EVENTS.LOGGED_OUT, this.user);
+        delete _private.$storage.user;
         delete this.user;
     }
 
